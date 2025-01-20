c1258cae1a44ca9af8828c5c5af55ba5002d4cf9
dcpetty <1700736+dcpetty@users.noreply.github.com>
2025-01-19 22:48:35 -0500

    Update to read .CSV files for data.

--- ccsae/sketch.js
+++ ccsae/sketch.js
@@ -1,66 +1,54 @@
 /*
  * CCSAE
  */
-const d = 30;
-const explainers = {
-  'a': [240, 035, ],
-  'b': [400, 100, ],  // [375, 130, ],
-  'c': [050, 215, ],
-  'd': [225, 235, ],
-  'e': [400, 235, ],
-  'f': [500, 235, ],
-  'g': [500, 335, ],
-  'h': [320, 430, ],
-  'i': [465, 430, ],
-  'j': [130, 555, ],
-  'k': [240, 555, ],
-  'l': [480, 555, ],
-  'm': [210, 770, ],
-  'n': [350, 770, ],
-};
-const experiments = {
-   1: [130, 105, 'ac', ],
-   2: [315, 075, 'ab', ],  // [315, 090, 'ab', ],
-   3: [320, 180, 'bd', ],
-   4: [400, 180, 'be', ],  // [385, 180, 'be', ],
-   5: [455, 180, 'bf', ],
-   6: [320, 235, 'de', ],
-   7: [295, 280, 'di', ],
-   8: [385, 280, 'el', ],
-   9: [475, 280, 'fh', ],
-  10: [525, 280, 'fg', ],
-  11: [285, 335, 'dh', ],
-  12: [585, 335, 'bl', ],
-  13: [025, 375, 'cm', ],
-  14: [120, 375, 'dj', ],
-  15: [170, 375, 'dm', ],
-  16: [215, 375, 'dk', ],
-  17: [490, 380, 'gi', ],
-  18: [400, 495, 'im', ],
-  19: [400, 550, 'lk', ],
-  20: [145, 615, 'jm', ],
-  21: [220, 615, 'km', ],
-  22: [295, 615, 'kn', ],
-  23: [355, 590, 'hn', ],
-  24: [400, 615, 'lm', ],
-  25: [425, 615, 'ln', ],
-  26: [265, 770, 'mn', ],
-};
+
+const canvasWidth = 600, canvasHeight = 800, d = 30;
+var explainers, experiments;
+
+/** Returns Object parsed from path csv file. For every nonempty line of 
+ * path file split on ',' after header, append to Object whose key is 
+ * first element of split line and whose value is rest of split line.
+ * @param {string} path path to csv file
+ * @returns {Object}
+ */
+async function parse(path) {
+  try {
+    let result = Object();
+    const response = await fetch(path);
+    const csv = await response.text();
+    const lines = csv.split(`\n`);
+    const header = lines[0].split(`,`);
+    console.assert(header.length > 0, `Missing header in '${path}'`)
+    console.log(`${path}: ${header}`);
+    // console.log(csv);
+    for ([i, l] of csv.split(`\n`).entries()) {
+      const tokens = l.split(`,`);
+      if (i > 0 && tokens.some((e) => e.length > 0)) {
+        result[tokens[0]] = tokens.slice(1);
+      }
+    }
+    return result;
+  } catch(err) {
+    console.log(err);
+  }
+}
 
 function path(v, d) {
-  const [x, y, p] = v;
-  const [s, e] = p, [sx, sy] = explainers[s], [ex, ey] = explainers[e];
+  const
+    [x, y, p] = v, [s, e] = p,
+    [sx, sy] = explainers[s], 
+    [ex, ey] = explainers[e];
   push();
   noFill();
   stroke('#639');
   strokeWeight(2);
   // Draw connection.
   beginShape();
-  curveVertex(sx, sy);
-  curveVertex(sx, sy);
-  curveVertex(x, y);
-  curveVertex(ex, ey);
-  curveVertex(ex, ey);
+  curveVertex(+sx, +sy);
+  curveVertex(+sx, +sy);
+  curveVertex(+x, +y);
+  curveVertex(+ex, +ey);
+  curveVertex(+ex, +ey);
   endShape();
   // https://editor.p5js.org/mahdadbor/sketches/evyWdjCSH
   // Draw arrowhead.
@@ -77,16 +65,38 @@ function disc(k, x, y, d, c) {
   push();
   strokeWeight(0);
   fill(c); // noFill();
-  ellipse(x, y, d);
+  ellipse(+x, +y, d);
   fill(0);
   textAlign(CENTER, CENTER);
-  text(k, x, y);
+  text(k, +x, +y);
   pop();
+}
+
+function hover(cx, cy, alt, d) {
+  push();
+  stroke('white');
+  fill('white');
+  rectMode(CENTER);
+  rect(+cx, +cy, d);
+  fill('black');
+  textAlign(CENTER, CENTER);
+  text(alt, +cx, +cy, d);
+  pop();
+}
+
+function isOver(obj, d) {
+  for (const [key, value] of Object.entries(obj)) {
+    const [x, y] = value;
+    if (dist(+x, +y, mouseX, mouseY) < d / 2) {
+      return key;
+    }
+  }
+  return undefined;
 }
 
 function echoMouse() {
   push();
-  fill(0);
+  fill('black');
   textAlign(CENTER, CENTER);
   if (mouseX > d / 2 && mouseY > d / 2)
     text(`(${mouseX}, ${mouseY})`, mouseX, mouseY);
@@ -94,15 +104,24 @@ function echoMouse() {
 }
 
 function setup() {
-  createCanvas(600, 800);
+  createCanvas(canvasWidth, canvasHeight);
+  // Anonymous async function for experiments promise.
+  (async () => {
+    experiments = await parse(`experiments.csv`);
+    console.log(experiments);
+    explainers = await parse(`explainers.csv`);
+    console.log(explainers);
+  })();
 }
 
 function draw() {
+  if (!experiments || !explainers) return;  // wait for initialization
+  // Redraw canvas.
   clear();
   background(240);
   // Draw paths for each experiment.
   for (const [key, value] of Object.entries(experiments)) {
-    path(value, d * 1.5);
+    if (value) path(value, d * 1.5);
   }
   // Draw experiment discs.
   for (const [key, value] of Object.entries(experiments)) {
@@ -114,9 +133,28 @@ function draw() {
     const [x, y, ] = value;
     disc(key, x, y, d * 1.5, 'gold');
   }
-  echoMouse();
+
+  let key;
+  // Check if mouse isOver an experiment and hover the alt text.
+  key = isOver(experiments, d);
+  if (key) {
+    const [cx, cy, p, u, a] = experiments[key];
+    hover(cx, cy, a, d * 3);
+  }
+  // Check if mouse isOver an explainer and hover the alt text.
+  key = isOver(explainers, d * 1.5);
+  if (key) {
+    const [cx, cy, u, a] = explainers[key];
+    hover(cx, cy, a, d * 3);
+  }
+  // Otherwise, echoMouse position.
+  if (!isOver(experiments, d) && !isOver(explainers, d * 1.5)) echoMouse();
 }
 
 function mouseClicked() {
-  window.open('https://google.com/', '_self');
+  let key;
+  key = isOver(experiments, d);
+  if (key) window.open(experiments[key][3], '_blank');
+  key = isOver(explainers, d * 1.5);
+  if (key) window.open(explainers[key][2], '_blank');
 }
